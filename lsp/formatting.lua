0938fc6d73a662f68164f0d9f1a6b88e16593b33
Alex Molchanov <amol4anov@mail.ru>
2023-03-26 00:39:14 +0300

    Add files to initial astronvim conf

---
+++ lsp/formatting.lua
@@ -0,0 +1,17 @@
+return {
+  format_on_save = {
+    enabled = true,
+    allow_filetypes = {
+      "lua",
+    },
+  },
+  timeout_ms = 3200, -- adjust the timeout_ms variable for formatting
+  disabled = { "lua_ls" },
+  filter = function(client)
+    -- only enable null-ls for javascript files
+    if vim.bo.filetype == "javascript" then return client.name == "null-ls" end
+
+    -- enable all other clients
+    return true
+  end,
+}
