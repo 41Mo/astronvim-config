33271b7ceb352d4c3717478f4fa5e452fc78dd30
Alex Molchanov <amol4anov@mail.ru>
2024-01-22 16:03:23 +0300

    add colorcolumn

--- options.lua
+++ options.lua
@@ -2,6 +2,11 @@ local function disable_editor_cfg()
 	vim.g.editorconfig = false
 end
 
+local function set_colorolumn()
+	vim.opt.colorcolumn = "80"
+end
+
 return {
 	disable_editor_cfg(),
+	set_colorolumn(),
 }
